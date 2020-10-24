2c8912cabe7ae13bce2cc3cfe1e905b2d47e6318
Jamile Radloff <jamilemarcele@gmail.com>
2020-10-24 17:38:06 +0200

    frontend in progress

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,7 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail, presence: true
   validates :cocktail_id, uniqueness: { scope: :ingredient_id }
 end
