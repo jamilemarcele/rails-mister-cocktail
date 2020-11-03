e68a891b91416e2c847d9e494f6fb9174a072a3f
Jamile Radloff <jamilemarcele@gmail.com>
2020-11-03 22:20:28 +0100

    adding reviews to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
+
   resources :cocktails, only: [:show, :new, :create, :destroy] do
     resources :doses, only: [:create]
+    resources :reviews, only: [:new, :creates]
   end
   resources :doses, only: [:destroy]
 end
