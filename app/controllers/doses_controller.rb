82c16d4f2db0dc0760b14110646d53d9590699ba
Jamile Radloff <jamilemarcele@gmail.com>
2020-11-13 16:44:22 +0100

    fixed doses create issue

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,12 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:create]
+  before_action :set_dose, only: [:destroy]
 
   def create
     @dose = Dose.new(dose_params)
     # we need `cocktail_id` to associate dose with corresponding cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-
+    @review = Review.new
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -14,13 +15,20 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @dose = Dose.find(params[:id])
     @dose.destroy
-
     redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
+
+  def set_cocktail
+    # we need `cocktail_id` to associate dose with corresponding cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_cocktail, only: [:new, :create]
-
-  def new
-    @review = Review.new
-  end
+  before_action :set_cocktail, only: [:create]
 
   def create
     @review = Review.new(review_params)
