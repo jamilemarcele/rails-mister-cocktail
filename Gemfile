af9d7ea6592f0bfb548814921754fc3afc378f90
Jamile Radloff <jamilemarcele@gmail.com>
2020-11-15 14:12:37 +0100

    create cocktail using cocktail api seeds

--- Gemfile
+++ Gemfile
@@ -51,5 +51,6 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
 gem 'faker'
+gem 'httparty'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.16.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,17 +9,45 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'open-uri'
 require 'json'
+require 'httparty'
 
 puts "Cleaning database..."
+Cocktail.destroy_all
 Ingredient.destroy_all
 
-puts "Starting seed 🌱"
+puts 'Starting seed 🌱'
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-json = open(url).read
-objs = JSON.parse(json)
-objs["drinks"].each do |obj|
-  Ingredient.create(name: obj["strIngredient1"])
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# json = open(url).read
+# objs = JSON.parse(json)
+# objs["drinks"].each do |obj|
+#   Ingredient.create(name: obj["strIngredient1"])
+# end
+
+puts 'Finished seeding file 😀'
+
+response = HTTParty.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
+
+cocktails = response.parsed_response['drinks']
+
+cocktails[0..2].each do |c|
+  c_id = c['idDrink']
+  cocktail_response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{c_id}")
+  cocktail_by_id = cocktail_response.parsed_response['drinks']
+  name = cocktail_by_id[0]['strDrink']
+
+  photo = URI.open(c['strDrinkThumb'])
+
+  cocktail = Cocktail.create!(name: name)
+  cocktail.photo.attach(io: photo, filename: "#{name}.jpeg", content_type: 'image/jpeg')
 end
 
-puts "Finished seeding file 😀"
+
+
+# json = open(url).read
+# objs = JSON.parse(json)
+# objs['drinks'].each do |obj|
+#   p obj['strDrink']
+#   p obj['strIngredient1']
+# end
+# Ingredient.create(name: obj["strIngredient1"])
